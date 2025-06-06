2cc5571920317a1cc320d45332d94bf12d926270
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-06 11:22:43 +0530

    Create list.tsx

---
+++ Common/list.tsx
@@ -0,0 +1,90 @@
+
+Here’s a comprehensive list which i recommend to learn:
+ 1. Array Buffer & Typed Arrays
+ 2. Array Destructuring
+ 3. Array Methods (map, filter & more)
+ 4. Arrow Functions Vs. Regular Functions
+ 5. Async / Await
+ 6. Bitwise Operators
+ 7. call(), apply(), bind()
+ 8. Callbacks
+ 9. Canvas API
+ 10. Clean Code Practices in JavaScript
+ 11. Client-Side Routing
+ 12. Closures
+ 13. Code Splitting
+ 14. Cross-Browser Compatibility
+ 15. Cross-Origin Resource Sharing (CORS)
+ 16. Currying
+ 17. Custom Events
+ 18. Debounce vs Throttle
+ 19. Debouncing and Throttling
+ 20. Deep vs. Shallow Copy
+ 21. Design Patterns (Observer, Singleton, Factory, etc.)
+ 22. Destructuring
+ 23. Destructuring Assignment
+ 24. Destructuring Nested Objects/Arrays
+ 25. DOM Manipulation
+ 26. Dynamic Imports
+ 27. Dynamic Typing
+ 28. Equality Operators (== vs ===)
+ 29. Error Boundaries (in React.js)
+ 30. Error Handling (Try/Catch/Throw)
+ 31. ES6 Features (Arrow Functions, Classes, Modules, Destructuring)
+ 32. Event Bubbling and Capturing
+ 33. Event Delegation
+ 34. Event Handling (addEventListener)
+ 35. Event Loop
+ 36. Fetch API
+ 37. Functions
+ 38. Generator Functions
+ 39. Geolocation API
+ 40. Geolocation vs Location Services
+ 41. Global and Local Object (window, globalThis)
+ 42. Hoisting
+ 43. IIFE (Immediately Invoked Function Expression)
+ 44. Inheritance (Class-based, Prototype-based)
+ 45. Intersection Observer API
+ 46. JavaScript Memory Management (Garbage Collection)
+ 47. JavaScript vs ECMAScript
+ 48. JSON (JavaScript Object Notation)
+ 49. Lazy Loading
+ 50. Map and Set
+ 51. Memoization
+ 52. Methods
+ 53. Module Pattern
+ 54. Modules (Import/Export)
+ 55. MutationObserver
+ 56. NaN (Not a Number)
+ 57. Object
+ 58. Object Literal Shorthand
+ 59. Object.assign()
+ 60. Performance Optimization
+ 61. Polyfills
+ 62. Promise.all()
+ 63. Promises
+ 64. Prototypal Inheritance
+ 65. RegEx (Regular Expressions)
+ 66. Scope (Function vs Block Scope)
+ 67. Service Workers
+ 68. Set and Map Iteration
+ 69. Set vs Map
+ 70. SetTimeout and SetInterval
+ 71. Shadow DOM
+ 72. Template Literals
+ 73. Shadowing
+ 74. Spread & Rest Operators
+ 75. Strict Mode
+ 76. SVG Manipulation
+ 77. Symbol
+ 78. String Concatenation
+ 79. This Keyword 
+ 80. Test-Driven Development
+ 81. Boolean Values
+ 82. let, var & const
+ 83. Type Coercion vs Type Conversion
+ 84. URL API (URLSearchParams, URL objects)
+ 85. WeakMap & WeakSet
+ 86. Web Animations
+ 87. localStorage & sessionStorage
+ 88. Web Workers & WebSockets
