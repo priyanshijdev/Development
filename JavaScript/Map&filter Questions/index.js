846fa6e182aeba9808329c3c9973a119385a7c0f
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-09 21:05:21 +0530

    Update index.js

--- JavaScript/Map&filter Questions/index.js
+++ JavaScript/Map&filter Questions/index.js
@@ -6,6 +6,7 @@ const updated= users.map((ele)=>{
 })
 
 // console.log('users-----', updated)
+---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Filter users by age greater than 25
    
@@ -30,9 +31,60 @@ const user = [
 
 const uniqueCities = [...new Set(user.map(user => user.city))];
 console.log('users----->>>', uniqueCities)
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
- // Output:
-//  [
-//   "Delhi",
-//   "Mumbai"
-// ]
+
+
+// Count how many users live in each city (using reduce)
+
+const users = [
+  { name: 'A', city: 'Delhi' },
+  { name: 'B', city: 'Mumbai' },
+  { name: 'C', city: 'Delhi' }
+];
+// Output: { Delhi: 2, Mumbai: 1 }
+
+// Answer---
+
+const cityCount = users.reduce((acc, user) => {
+  acc[user.city] = (acc[user.city] || 0) + 1;
+  return acc;
+}, {});
+
+console.log('cityCount----', cityCount)
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Group objects by a key (e.g., department)
+
+const employees = [
+  { name: 'Alice', dept: 'HR' },
+  { name: 'Bob', dept: 'Engineering' },
+  { name: 'Eve', dept: 'HR' }
+];
+
+
+// Output: {
+//   HR: [{ name: 'Alice', dept: 'HR' }, { name: 'Eve', dept: 'HR' }]
+//   Engineering: [{ name: 'Bob', dept: 'Engineering' }]
+// }
+
+const updatedEmp= employees.reduce((acc,emp)=> {
+  if(!acc[emp.dept]){
+    acc[emp.dept] = [] 
+  }
+  acc[emp.dept].push(emp);
+  return acc;
+})
+
+console.log('updatedEmp =', updatedEmp)
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+ 
