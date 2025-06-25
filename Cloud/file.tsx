78e67b9b656074187c5e7ce38f3a1ead8b0bfff0
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-25 16:06:36 +0530

    Create file.tsx

---
+++ Cloud/file.tsx
@@ -0,0 +1,14 @@
+Q1. What is Docker and why do we use it?
+
+  Docker is a platform that allows you to package applications and their dependencies into containers. 
+A container is a lightweight, standalone, and executable unit that includes everything needed to run the application—code, runtime, libraries, and system tools.
+
+Q2. Why we use Docker:
+
+Ensures consistency across environments (dev, test, production).
+
+Simplifies deployment and scaling.
+
+Enables isolation and resource efficiency.
+
+Speeds up setup and collaboration.
