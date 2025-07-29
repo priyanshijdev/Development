d89f325bd9dc9bb37656fc003cbd13d1dbc4ea44
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-07-29 19:22:51 +0530

    Create optimisation_query.js

---
+++ Backend/optimisation_query.js
@@ -0,0 +1,17 @@
+Key Takeaways:
+
+  Analyze Execution Plans
+  Use Indexes Effectively
+  Optimize Data Retrieval
+  Refine JOIN Operations
+  Use LIMIT and OFFSET
+  Avoid SELECT DISTINCT
+  Limit Use of Subqueries
+  Choose EXISTS Over IN
+  Implement Efficient Pagination
+  Minimize OR Conditions
+  Avoid Unnecessary Calculations
+  Regular Review and Refinement
+  Understand Data and Requirements
+  Optimize Use of Temporary Tables
+
