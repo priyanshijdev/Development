f30331a90f34510ac26da861d8d2bf7c71a7ec7f
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-15 22:42:03 +0530

    Create essentialQuetions.ts

---
+++ ReactJs/interview/essentialQuetions.ts
@@ -0,0 +1,31 @@
+𝗘𝘀𝘀𝗲𝗻𝘁𝗶𝗮𝗹 𝗥𝗲𝗮𝗰𝘁 𝗜𝗻𝘁𝗲𝗿𝘃𝗶𝗲𝘄 𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻𝘀
+1. What is the difference between React Node, Element, and Component?
+2. What are React Fragments used for?
+3. What is the purpose of the key prop in React?
+4. What is the consequence of using array indices as keys?
+5. What is the difference between Controlled and Uncontrolled components?
+6. How would you lift state up and why is it necessary?
+7. What are Pure Components?
+8. What is the difference between createElement and cloneElement?
+9. What is the role of PropTypes in React?
+10. What are stateless components?
+11. What are stateful components?
+12. What are the benefits of using hooks in React?
+13. What are the rules of React hooks?
+14. What is the difference between useEffect and useLayoutEffect?
+15. What does the dependency array of useEffect affect?
+16. What is the useRef hook and when should it be used?
+17. Why does React recommend against mutating state?
+18. What is reconciliation in React?
+19. What is hydration in React?
+20. Explain higher-order components (HOCs)
+21. What are some common performance optimization techniques in React?
+22. What is the purpose of the useReducer hook in React?
+23. How do you test a React application?
+24. What is server-side rendering (SSR)?
+25. What is Static site generation (SSG)?
+26. What is lazy loading in React?
+27. What is the purpose of the useContext hook in React?
+28. What is the purpose of the useMemo hook in React?
+29. What is the purpose of the useCallback hook in React?
+30. What is the purpose of the useImperativeHandle hook in React?
