05fcf4c80147515da66765fdfefb25e4f4dfdad2
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-23 16:33:45 +0530

    Create JavaScript.js

---
+++ Basics/JavaScript.js
@@ -0,0 +1,13 @@
+JavaScript is a dynamic, interpreted programming language primarily known for enabling interactive and dynamic content on web pages.
+  It is one of the three core technologies of the World Wide Web, working alongside HTML (for structure) and CSS (for styling).
+
+JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and
+pretty much everything else. (Okay, not everything, but it is amazing what you can achieve with a few lines of JavaScript code.)
+
+  
+  Key characteristics and uses of JavaScript:
+
+1. Client-side scripting:
+2.Server-side development:
+3.Rich ecosystem:
+4. Dynamic
