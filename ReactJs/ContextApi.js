a4a3db6f45aea6463c99da1a14fd73958693e76e
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-25 21:34:00 +0530

    Create ContextApi.js

---
+++ ReactJs/ContextApi.js
@@ -0,0 +1,7 @@
+The Context API in React is a feature that enables sharing data across components without the need for "prop drilling," 
+  which is the process of passing props down through multiple levels of the component tree. 
+It provides a way to make data accessible to any component within a specific part of your application, regardless of its nesting depth. 
+
+    
+
+  
