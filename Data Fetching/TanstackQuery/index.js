79b5d78a26b110ea2ba7cbfebd771d8fa7e09708
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-09-08 13:55:50 +0530

    Create index.js

---
+++ Data Fetching/TanstackQuery/index.js
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
