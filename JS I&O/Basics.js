5c66589c912f43910033b09c8d6cf36d37b6c49d
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-09-28 23:41:39 +0530

    Create Basics.js

---
+++ JS I&O/Basics.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3];   // arr = [1,2,3]
+
+arr.length = 0;      // clears the array, arr = []
+
+arr.push(4);         // arr = [4]
+
+console.log(arr[0], arr.length);
+
+
+Output  4 0 
